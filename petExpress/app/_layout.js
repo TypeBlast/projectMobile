c8553b19aec9f231a6705f458e57302116f5c3e2
dePaula06 <p.depaula06.sp@gmail.com>
2024-08-18 18:50:24 -0300

    CreateApp

---
+++ petExpress/app/_layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+// Importe das páginas
+
+
+export default function Layout() {
+  return (
+    <View>
+      <Text>Oiiiiiii</Text>
+    </View>
+  );
+}
