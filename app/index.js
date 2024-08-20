7d7c7e1b50d515a2ab2380ab538d84fffdba20b0
dePaula06 <p.depaula06.sp@gmail.com>
2024-08-20 12:46:55 -0300

    Create app 2.0

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>OLA</Text>
+      <Text>OLA</Text>
+      <Text>OLA</Text>
+      <Text>OLA</Text>
+    </View>
+  );
+}
