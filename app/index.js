fd9e64281d6c54315c72fc4adf2629840f831a77
dePaula06 <p.depaula06.sp@gmail.com>
2024-08-21 10:05:21 -0300

    Configurando pilha de navegação

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+//Import components
+
+
+function Index() {
+
+
+    return(
+        <View>
+            <Text>OIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII</Text>
+        </View>
+    );
+
+}
+
+export default Index;
