7596201219ace5e48011cca946c19fe91e7fa5c6
Alunods <vinsouzza@gmail.com>
2024-08-22 15:00:46 -0300

    feat: implements handleRegister

--- app/register.js
+++ app/register.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { View, StyleSheet, Image, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Image, Text, TouchableOpacity, Alert } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import axios from '../services/api'; // Importe o Axios configurado
 
-//Import components
+// Import components
 import InputEmail from "../components/inputs/inputEmail";
 import InputCPF from "../components/inputs/inputCPF";
 import InputName from "../components/inputs/inputName";
@@ -9,6 +11,30 @@ import InputPhone from "../components/inputs/inputPhone";
 import InputKey from "../components/inputs/inputKey";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  const handleRegister = async () => {
+    try {
+      const user = { email, cpf, name, phone, password };
+      const response = await axios.postUser(user); // Certifique-se de que o endpoint está correto
+
+      if (response.status === 201) {
+        Alert.alert("Sucesso", response.data.message);
+        navigation.navigate('index'); 
+        // Opcional: Navegue para outra página após o registro
+      } else {
+        Alert.alert("Erro", response.data.message);
+      }
+    } catch (error) {
+      Alert.alert("Erro", error.response.data.message);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Crie sua conta!</Text>
@@ -19,12 +45,13 @@ function Register() {
       <Text style={styles.textWelcome}>
         Cadastre-se e desfrute de nossos serviços!
       </Text>
-      <InputEmail />
-      <InputCPF />
-      <InputName />
-      <InputPhone />
-      <InputKey />
-      <TouchableOpacity style={styles.buttonRegister}>
+      <InputEmail value={email} onChangeText={setEmail} />
+      <InputCPF value={cpf} onChangeText={setCpf} />
+      <InputName value={name} onChangeText={setName} />
+      <InputPhone value={phone} onChangeText={setPhone} />
+      <InputKey value={password} onChangeText={setPassword} secureTextEntry />
+
+      <TouchableOpacity style={styles.buttonRegister} onPress={handleRegister}>
         <Text style={styles.textButtonRegister}>Cadastrar</Text>
       </TouchableOpacity>
     </View>
@@ -36,12 +63,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFF",
     flex: 1,
     alignItems: "center",
-  },
-  backButton: {
-    width: "4%",
-    height: "3%",
-    marginTop: "25%",
-    marginLeft: "-85%",
   },
   title: {
     fontFamily: "Poppins-Bold",
@@ -64,15 +85,16 @@ const styles = StyleSheet.create({
     height: "7%",
     backgroundColor: '#EB389A',
     borderRadius: 10,
-    marginTop: "10%"
-},
-textButtonRegister: {
+    marginTop: "10%",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textButtonRegister: {
     fontFamily: 'Poppins-Bold',
     fontSize: 24,
     color: "#FFF",
     textAlign: 'center',
-    marginTop: "2.5%"
-},
+  },
 });
 
 export default Register;
