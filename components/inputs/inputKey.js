5665837faa9f3c47fbafc948694591eb6eadfb88
Alunods <vinsouzza@gmail.com>
2024-08-27 15:02:05 -0300

    chore:implementation of the see password function

--- components/inputs/inputKey.js
+++ components/inputs/inputKey.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faKey } from '@fortawesome/free-solid-svg-icons';
+import Feather from '@expo/vector-icons/Feather';
 
-function InputKey({ value, onChangeText, secureTextEntry }) {
+function InputKey({ value, onChangeText }) {
+    const [secureTextEntry, setSecureTextEntry] = useState(true);
+    const [isInputFilled, setIsInputFilled] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setSecureTextEntry(!secureTextEntry);
+    };
+
+    const handleTextChange = (text) => {
+        onChangeText(text);
+        setIsInputFilled(text.length > 0); // Verifica se o campo tem texto
+    };
+
     return (
         <View style={styles.container}>
             <FontAwesomeIcon icon={faKey} color='#D9D9D9' size={20} style={styles.icon} />
@@ -12,9 +25,14 @@ function InputKey({ value, onChangeText, secureTextEntry }) {
                 placeholder="Senha"
                 keyboardType="default"
                 value={value}
-                onChangeText={onChangeText}
+                onChangeText={handleTextChange}
                 secureTextEntry={secureTextEntry}
             />
+            {isInputFilled && (
+                <TouchableOpacity onPress={togglePasswordVisibility} style={styles.iconEye}>
+                    <Feather name={secureTextEntry ? "eye-off" : "eye"} size={24} color="black" />
+                </TouchableOpacity>
+            )}
         </View>
     );
 }
@@ -22,16 +40,23 @@ function InputKey({ value, onChangeText, secureTextEntry }) {
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
+        alignItems: 'center',
         marginTop: "5%",
     },
     input: {
         borderBottomColor: "#D9D9D9",
         borderBottomWidth: 1.5,
         width: "70%",
+        paddingRight: 30, // Espaço extra para o ícone do olho
     },
     icon: {
         marginTop: "2%",
         marginRight: "2%",
+    },
+    iconEye: {
+        position: 'absolute',
+        right: 0,
+        padding: 10,
     },
 });
 
