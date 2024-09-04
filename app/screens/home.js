a2d9347eb7757b77348398034ae376fb9d1766fb
vinicius souza <vinicius.souza.oliveira123@gmail.com>
2024-09-03 22:04:49 -0300

    chore: continuation of the home page styling (not yet finished)

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,5 +1,12 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { UserContext } from "../UserContext"; // Importe o UserContext
 
 function HomePage() {
@@ -31,23 +38,69 @@ function HomePage() {
       <View style={styles.Categorias}>
         <Text style={styles.textCategorias}>Categorias</Text>
         {/* Adicionando ScrollView horizontal */}
-        <ScrollView 
-          horizontal 
-          showsHorizontalScrollIndicator={false} 
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.buttonsContainer}
         >
           <TouchableOpacity style={styles.Alimentacao}>
             <Text style={styles.textAlimentacao}>Alimentação</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.Alimentacao}>
-            <Text style={styles.textAlimentacao}>Brinquedos</Text>
+          <TouchableOpacity style={styles.Beleza}>
+            <Text style={styles.textAlimentacao}>Higiene e beleza</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.Alimentacao}>
+          <TouchableOpacity style={styles.Acessorios}>
             <Text style={styles.textAlimentacao}>Acessórios</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.Alimentacao}>
-            <Text style={styles.textAlimentacao}>Higiene</Text>
+          <TouchableOpacity style={styles.Saude}>
+            <Text style={styles.textAlimentacao}>Saúde</Text>
           </TouchableOpacity>
+        </ScrollView>
+      </View>
+      <View style={styles.Especies}>
+        <Text style={styles.textCategorias}>Espécies</Text>
+        {/* Adicionando ScrollView horizontal para as espécies */}
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.speciesContainer}
+        >
+          <View style={styles.viewDog}>
+            <TouchableOpacity style={styles.Cachorro}>
+              <Image
+                source={require("../../assets/images/dogImage1.png")}
+                style={styles.imageDog}
+              />
+            </TouchableOpacity>
+            <Text style={styles.textCachorro}>Cachorro</Text>
+          </View>
+          <View style={styles.viewCat}>
+            <TouchableOpacity style={styles.Gato}>
+              <Image
+                source={require("../../assets/images/catImage1.png")}
+                style={styles.imageDog}
+              />
+            </TouchableOpacity>
+            <Text style={styles.textGato}>Gato</Text>
+          </View>
+          <View style={styles.viewBird}>
+            <TouchableOpacity style={styles.Bird}>
+              <Image
+                source={require("../../assets/images/dogImage1.png")}
+                style={styles.imageDog}
+              />
+            </TouchableOpacity>
+            <Text style={styles.textBird}>Pássaro</Text>
+          </View>
+          <View style={styles.viewReptil}>
+            <TouchableOpacity style={styles.Reptil}>
+              <Image
+                source={require("../../assets/images/catImage1.png")}
+                style={styles.imageDog}
+              />
+            </TouchableOpacity>
+            <Text style={styles.textReptil}>Réptil</Text>
+          </View>
         </ScrollView>
       </View>
     </View>
@@ -98,16 +151,16 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   Pet: {
-    fontSize: 17,
+    fontSize: 16,
     fontFamily: "MonumentExtentend",
     color: "#000",
   },
   Texto: {
-    fontSize: 17,
+    fontSize: 15,
     color: "#000",
     fontFamily: "Poppins-Regular",
     top: 5,
-    textAlign: "start", // Centraliza o texto na View
+    textAlign: "start",
   },
   welcomeText: {
     fontSize: 24,
@@ -117,33 +170,144 @@ const styles = StyleSheet.create({
     left: 20,
   },
   Categorias: {
-    backgroundColor: "white",
     top: "10%",
-    height: 150, // Aumentei a altura para caber os botões
+    height: 100,
   },
   textCategorias: {
-    color:"#EB389A",
-    fontFamily:"Poppins-SemiBold",
-    fontSize:18,
-    left:35,
+    color: "#EB389A",
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 18,
+    left: 35,
   },
   buttonsContainer: {
-    flexDirection: "row", // Alinha os botões horizontalmente
-    paddingHorizontal: 30, // Mantém o espaçamento lateral
+    flexDirection: "row",
+    paddingHorizontal: 25,
     paddingVertical: 10,
   },
   Alimentacao: {
     backgroundColor: "#CE88F8",
     height: 40,
-    width: 120,
+    width: 125,
     borderRadius: 10,
-    marginHorizontal: 10, // Espaçamento entre os botões
-    alignItems: 'center',
-    justifyContent: 'center'
+    marginHorizontal: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Beleza: {
+    backgroundColor: "#FF8DBA",
+    height: 40,
+    width: 125,
+    borderRadius: 10,
+    marginHorizontal: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Acessorios: {
+    backgroundColor: "#85F78D",
+    height: 40,
+    width: 125,
+    borderRadius: 10,
+    marginHorizontal: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Saude: {
+    backgroundColor: "#FDFF8A",
+    height: 40,
+    width: 125,
+    borderRadius: 10,
+    marginHorizontal: 5,
+    alignItems: "center",
+    justifyContent: "center",
   },
   textAlimentacao: {
     color: "#fff",
     fontFamily: "Poppins-Bold",
+  },
+  Especies: {
+    top: "10%",
+    height: 180,
+  },
+  speciesContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  viewDog: {
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  imageDog: {
+    height: 80,
+    width: 80,
+  },
+  Cachorro: {
+    height: 100,
+    width: 100,
+    borderRadius: 8,
+    borderWidth: 4,
+    borderColor: "#A63DDC",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textCachorro: {
+    color: "#A63DDC",
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 16,
+  },
+  viewCat: {
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  Gato: {
+    height: 100,
+    width: 100,
+    borderRadius: 8,
+    borderWidth: 4,
+    borderColor: "#FF423D",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textGato: {
+    color: "#FF423D",
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 16,
+  },
+  viewBird: {
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  Bird: {
+    height: 100,
+    width: 100,
+    borderRadius: 8,
+    borderWidth: 4,
+    borderColor: "#37E843",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textBird: {
+    color: "#37E843",
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 16,
+  },
+  viewReptil: {
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  Reptil: {
+    height: 100,
+    width: 100,
+    borderRadius: 8,
+    borderWidth: 4,
+    borderColor: "#FCFF3D",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textReptil: {
+    color: "#FCFF3D",
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 16,
   },
 });
 
